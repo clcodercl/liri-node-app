08e5583e362f0545a08c6637ce136dd00e7015f4
Cynthia London <cynthe@sbcglobal.net>
2016-10-15 20:29:02 -0500

    added these files to the wrong directory--adding them in now

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'dCugRxRkZhUH1BjSvuZkgoZBC',
+  consumer_secret: 'lH0bKxh1q9JK5UJPaq2QFHLZQ4GAVbwr8OvDKu5biy8IMytgsr',
+  access_token_key: '785617807748702208-Qz03mdEuEmvR7FkLXnsWQYyYs9xEYgt',
+  access_token_secret: '6vGac8ryIYpSgxdJYVwcn9d0BFPcY9KFvR1Vg3sbDmmhU',
+}
