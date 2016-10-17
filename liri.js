c05741c534fa816dbe7e6c8ac82f304c74f8cdcc
Cynthia London <cynthe@sbcglobal.net>
2016-10-17 07:06:59 -0500

    added some stuff

--- liri.js
+++ liri.js
@@ -1,17 +1,45 @@
-//my-tweets
+var request = require('request');
+var spotify = require('spotify');
+var operator = process.argv[2];
+var fs = require('fs');
+//At the top of the liri.js file, write the code you need to grab the data from keys.js. 
+
+var keys = require(./keys.js);
+//Then store the keys in a variable.
+
+fs.watch(keys.js, function() {
+	let ls spawn('ls', ['-lh', key.js]);
+	output ='';
+	ls.stdout.on('data', function(chunk){
+		output += chunk.tostring();
+	});
+
+	ls.on('close', function(){
+});
 
 
-//spotify-this-song
 
 
-//movie-this
+//Make it so liri.js can take in one of the following commands:
+function commands() {
+  this.my-tweets = [],
+  this.spotify-this-song = [],
+  this.movie-this
+  this.do-what-it-says
+	//my-tweets
 
+	//spotify-this-song
 
-//do-what-it-says
+	//movie-this
 
-//show your last 20 tweets and when they were created at in your terminal/bash window.
-	//node liri.js my-tweets
+	//do-what-it-says
 
+// What Each Command Should Do
+
+// node liri.js my-tweets
+
+// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+// node liri.js spotify-this-song '<song name here>'
 
 
 // This will show the following information about the song in your terminal/bash window
@@ -20,13 +48,10 @@
 // The song's name
 // A preview link of the song from Spotify
 // The album that the song is from
-	//node liri.js spotify-this-song '<song name here>'
+// if no song is provided then your program will default to
 
-//if no song is provided then your program will default to
-
-//"The Sign" by Ace of Base
-
-//node liri.js movie-this '<movie name here>'
+// "The Sign" by Ace of Base
+// node liri.js movie-this '<movie name here>'
 
 // This will output the following information to your terminal/bash window:
 
@@ -39,17 +64,8 @@
 // Actors in the movie.
 // Rotten Tomatoes Rating.
 // Rotten Tomatoes URL.
+// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
-//node liri.js do-what-it-says
-
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-// Feel free to change the text in that document to test out the feature for other commands.
-
-//BONUS
-
-// In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
-
-// Make sure you append each command you run to the log.txt file.
-
-// Do not overwrite your file each time you run a command.
+// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+// It's on Netflix!
+// node liri.js do-what-it-says
